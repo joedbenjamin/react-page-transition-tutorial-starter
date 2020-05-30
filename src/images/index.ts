1371ecf2807c9a95e82aa3bdac803e9f606cb3d8
joedbenjamin <joedbenjamin@gmail.com>
2020-05-30 02:06:45 -0400

    adding in images

---
+++ src/images/index.ts
@@ -0,0 +1,8 @@
+import image1 from './ricardo-velarde-3jTWg5hHuVQ-unsplash.jpg';
+import image2 from './camilla-carvalho-Y2_V49CR6fE-unsplash.jpg';
+import image3 from './kendal--uIDAgR9rhY-unsplash.jpg';
+import image4 from './davids-kokainis-XJ92m7W6IXY-unsplash.jpg';
+
+const images = [image1, image2, image3, image4];
+
+export { images };
